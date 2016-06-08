05ef05c1ac4a7e60db88b0050c3251eb537b43c6
Ethan Cohen <ethanvcohen@gmail.com>
2016-06-08 13:42:13 -0400

    [new] - getAttribute implemented
    returns the actual attribute object if it exists.

--- src/index.js
+++ src/index.js
@@ -1,7 +1,9 @@
 import hasAttribute from './hasAttribute';
 import getNodeType from './getNodeType';
+import getAttribute from './getAttribute';
 
 module.exports = {
   hasAttribute,
   getNodeType,
+  getAttribute,
 };
