b63574aa0e5f7e17b5dfd781e15b9761ce75b4cb
Victor Magalhães <victorhfmag@gmail.com>
2019-02-06 15:18:15 -0300

    Provide a mechanism for tests to set their desired parser plugins

--- __tests__/helper.js
+++ __tests__/helper.js
@@ -2,16 +2,27 @@ import getProp from '../src/getProp';
 
 const parser = require('@babel/parser');
 
+const defaultPlugins = ['jsx', 'functionBind', 'estree', 'objectRestSpread', 'optionalChaining'];
+let plugins = [...defaultPlugins];
+
+export function changePlugins(pluginOrFn) {
+  if (Array.isArray(pluginOrFn)) {
+    plugins = pluginOrFn;
+  } else if (typeof pluginOrFn === 'function') {
+    plugins = pluginOrFn(plugins);
+  } else {
+    throw new Error('changePlugins argument should be either an array or a function');
+  }
+}
+
+/* eslint-disable */
+beforeEach(() => {
+  plugins = [...defaultPlugins];
+});
+/* eslint-enable */
+
 function parse(code) {
-  return parser.parse(code, {
-    plugins: [
-      'estree',
-      'functionBind',
-      'jsx',
-      'objectRestSpread',
-      'optionalChaining',
-    ],
-  });
+  return parser.parse(code, { plugins });
 }
 
 export function getOpeningElement(code) {
